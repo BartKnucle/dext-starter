8207640ce0c5c98ec8124f9a69e2e14070266cf0
BartKnucle <sjarde@yahoo.fr>
2019-01-24 17:34:27 +0100

    Clean test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,34 +1,11 @@
 <template>
-  <v-layout>
-    <ul>
-      <li>
-        {{ data }}
-      </li>
-      <li>
-        {{ item }}
-      </li>
-    </ul>
-  </v-layout>
+  <v-layout/>
 </template>
 <script>
-import { MODULE } from '../lib/module.js'
 export default {
   data: () => {
-    return {
-      test: {},
-      data: {},
-      item: []
-    }
+    return {}
   },
-  mounted: async function() {
-    console.log(this.app)
-    this.test = new MODULE(
-      this,
-      'QmbKCVVjmnutiddn8KFmCczMomDt1exAWWUaa2kpg4Hrex'
-    )
-    await this.test.init()
-    this.data = this.test.all()
-    //this.item = this.test.get('test')[0].data.variable1
-  }
+  mounted: async function() {}
 }
 </script>
