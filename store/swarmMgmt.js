d247fb45de13392fa24e2e717030164bfe300422
BartKnucle <sjarde@yahoo.fr>
2019-03-21 16:34:33 +0100

    Added getters to the swarmMgmt store

--- store/swarmMgmt.js
+++ store/swarmMgmt.js
@@ -7,6 +7,12 @@ export const getters = {
   nodes: state => {
     return state.nodes
   },
+  computers: state => {
+    return state.nodes.filter(node => node.type === 'computer')
+  },
+  users: state => {
+    return state.nodes.filter(node => node.type === 'user')
+  },
   nameByID: state => id => {
     let node = state.nodes.find(node => node.id === id)
     if (node) {
