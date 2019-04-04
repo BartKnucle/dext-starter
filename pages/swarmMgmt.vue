f2b27017acdc659e5a2705ae1aad089be10524d7
BartKnucle <sjarde@yahoo.fr>
2019-04-04 10:21:32 +0200

    Added users filter

--- pages/swarmMgmt.vue
+++ pages/swarmMgmt.vue
@@ -76,7 +76,35 @@
         Users
       </v-tab>
       <v-tab-item>
-        Users
+        <v-container
+          fluid
+          grid-list-md>
+          <v-data-iterator
+            :items="$store.getters['swarmMgmt/users']"
+            content-tag="v-layout"
+            hide-actions
+            row
+            wrap>
+            <v-flex
+              slot="item"
+              slot-scope="props"
+              xs12
+              sm6
+              md4
+              lg3>
+              <v-card
+                :color="nodeTypeColor(props.item.type)">
+                <v-card-title>
+                  <v-icon
+                    large>
+                    {{ nodeTypeIcon(props.item.type) }}
+                  </v-icon>
+                  <h4>{{ props.item.name }}</h4>
+                </v-card-title>
+              </v-card>
+            </v-flex>
+          </v-data-iterator>
+        </v-container>
       </v-tab-item>
     </v-tabs>
   </v-container>
